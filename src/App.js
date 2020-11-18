201dc08a7473849bb0ce9eee11e744e1790e9fc3
will <swdust@gmail.com>
2020-11-18 14:35:20 -0300

    firebase integration and user authentication

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
+
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+
+import { useAuthState } from 'react-firebase-hooks/auth'
+import { useCollectionData } from 'react-firebase-hooks/firestore'
+
+firebase.initializeApp({
+  apiKey: 'AIzaSyCBBUpAjz9_E6bmchDsRu-v2AkB8ccGkwg',
+  authDomain: 'unkchat-byuiu.firebaseapp.com',
+  databaseURL: 'https://unkchat-byuiu.firebaseio.com',
+  projectId: 'unkchat-byuiu',
+  storageBucket: 'unkchat-byuiu.appspot.com',
+  messagingSenderId: '85909992147',
+  appId: '1:85909992147:web:ceaf2af6e7f65b2e011a26',
+})
 
 function App() {
+  const [user] = useAuthState(auth)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <header className='App-header'></header>
+      <section>{user ? <ChatRoom /> : <SignIn />}</section>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
