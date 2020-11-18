89b786f48a0aaec35db29bc32ded6070adc1ffcd
will <swdust@gmail.com>
2020-11-18 18:52:41 -0300

    v1.0

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import logo from './logo.svg'
+import React, { useRef, useState } from 'react'
 import './App.css'
 
 import firebase from 'firebase/app'
 import 'firebase/firestore'
 import 'firebase/auth'
+import 'firebase/analytics'
 
 import { useAuthState } from 'react-firebase-hooks/auth'
 import { useCollectionData } from 'react-firebase-hooks/firestore'
@@ -15,16 +16,119 @@ firebase.initializeApp({
   projectId: 'unkchat-byuiu',
   storageBucket: 'unkchat-byuiu.appspot.com',
   messagingSenderId: '85909992147',
-  appId: '1:85909992147:web:ceaf2af6e7f65b2e011a26',
+  appId: '1:85909992147:web:f6fd52d5a8f26184011a26',
 })
+
+const auth = firebase.auth()
+const firestore = firebase.firestore()
+const analytics = firebase.analytics()
 
 function App() {
   const [user] = useAuthState(auth)
+
   return (
     <div className='App'>
-      <header className='App-header'></header>
+      <header>
+        <img
+          src='https://media1.tenor.com/images/321d2c7ca417df4853cbdac646d19b83/tenor.gif?itemid=7216186'
+          alt=''
+        />
+        <SignOut />
+      </header>
+
       <section>{user ? <ChatRoom /> : <SignIn />}</section>
     </div>
+  )
+}
+
+function SignIn() {
+  const signInWithGoogle = () => {
+    const provider = new firebase.auth.GoogleAuthProvider()
+    auth.signInWithPopup(provider)
+  }
+
+  return (
+    <>
+      <button className='sign-in' onClick={signInWithGoogle}>
+        Entre se quiser com Google :D <br />
+        Se não quiser não entre :D
+      </button>
+      <p></p>
+    </>
+  )
+}
+
+function SignOut() {
+  return (
+    auth.currentUser && (
+      <button className='sign-out' onClick={() => auth.signOut()}>
+        Se saia
+      </button>
+    )
+  )
+}
+
+function ChatRoom() {
+  const dummy = useRef()
+  const messagesRef = firestore.collection('messages')
+  const query = messagesRef.orderBy('createdAt').limit(25)
+
+  const [messages] = useCollectionData(query, { idField: 'id' })
+
+  const [formValue, setFormValue] = useState('')
+
+  const sendMessage = async (e) => {
+    e.preventDefault()
+
+    const { uid, photoURL } = auth.currentUser
+
+    await messagesRef.add({
+      text: formValue,
+      createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+      uid,
+      photoURL,
+    })
+
+    setFormValue('')
+    dummy.current.scrollIntoView({ behavior: 'smooth' })
+  }
+
+  return (
+    <>
+      <main>
+        {messages &&
+          messages.map((msg) => <ChatMessage key={msg.id} message={msg} />)}
+
+        <span ref={dummy}></span>
+      </main>
+
+      <form onSubmit={sendMessage}>
+        <input
+          value={formValue}
+          onChange={(e) => setFormValue(e.target.value)}
+          placeholder='fale se quiser'
+        />
+
+        <button type='submit' disabled={!formValue}>
+          🤟🏻
+        </button>
+      </form>
+    </>
+  )
+}
+
+function ChatMessage(props) {
+  const { text, uid, photoURL } = props.message
+
+  const messageClass = uid === auth.currentUser.uid ? 'sent' : 'received'
+
+  return (
+    <>
+      <div className={`message ${messageClass}`}>
+        <img src={photoURL} />
+        <p>{text}</p>
+      </div>
+    </>
   )
 }
 
